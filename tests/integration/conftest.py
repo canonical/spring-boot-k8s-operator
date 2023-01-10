2f734b02cb5e161e2c0c5c8394e7cfb3b63a8ed5
Weii Wang <weii.wang@canonical.com>
2023-01-10 16:38:16 +0800

    Add integration tests for buildpack image

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -9,11 +9,12 @@ import pytest_operator.plugin
 
 @pytest_asyncio.fixture(scope="module", name="get_unit_ip_list")
 async def fixture_get_unit_ip_list(ops_test: pytest_operator.plugin.OpsTest):
-    """Helper function to retrieve unit ip addresses, similar to fixture_get_unit_status_list"""
+    """Helper function to retrieve unit ip addresses."""
 
-    async def _get_unit_ip_list():
+    async def _get_unit_ip_list(app_name: str):
+        assert ops_test.model
         status = await ops_test.model.get_status()
-        units = status.applications["spring-boot-k8s"].units
+        units = status.applications[app_name].units
         ip_list = []
         for key in sorted(units.keys(), key=lambda n: int(n.split("/")[-1])):
             ip_list.append(units[key].address)
